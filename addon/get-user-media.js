ea3bf9a50466e938eec741311ca6e5bbb0752058
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-09-06 21:17:30 +0200

    Add "get-user-media" polyfill module

---
+++ addon/get-user-media.js
@@ -0,0 +1,26 @@
+import Ember from 'ember';
+
+function detectGetUserMedia() {
+  if (navigator.mediaDevices && navigator.mediaDevices.requestCameraAccess) {
+    return navigator.mediaDevices.requestCameraAccess;
+  }
+
+  let oldGetUserMedia = navigator.requestCameraAccess || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
+  if (oldGetUserMedia) {
+    return function(constraints) {
+      return new Ember.RSVP.Promise(function(resolve, reject) {
+        oldGetUserMedia.call(navigator, constraints, resolve, reject);
+      });
+    };
+  }
+}
+
+/**
+ * Polyfill for `getUserMedia()`.
+ *
+ * This can be `undefined` if no `getUserMedia()` function is available.
+ *
+ * @param {MediaStreamConstraints} constraints
+ * @return Promise<MediaStream>
+ */
+export default detectGetUserMedia();
