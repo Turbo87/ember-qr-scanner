a727b4676ef13d41b1525acdc85094d6be807707
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-09-06 13:43:00 +0200

    Add vendored "jsqrcode" library

--- index.js
+++ index.js
@@ -2,5 +2,10 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-qr-scanner'
+  name: 'ember-qr-scanner',
+
+  included: function() {
+    this.import('vendor/jsqrcode.js');
+    this.import('vendor/shims/jsqrcode.js');
+  },
 };
